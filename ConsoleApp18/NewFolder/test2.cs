1a15732885da0b49a5d81314e093ddd1fe5a3481
Chen An <v-chenan1@microsoft.com>
2021-09-24 03:14:31 +0000

    8520

---
+++ ConsoleApp18/NewFolder/test2.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp18.NewFolder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class test2
+    {
+    }
+}
